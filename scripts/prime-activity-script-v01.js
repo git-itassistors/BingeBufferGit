f817bfa45944acd49f95127a8d1ac4f6abfdb78e
itassistors <itassistors2013@gmail.com>
2018-10-20 16:08:02 +0530

    Header search
    Searchbar added to the bottom of the header navbar

--- scripts/prime-activity-script-v01.js
+++ scripts/prime-activity-script-v01.js
@@ -7,14 +7,25 @@ Date : 19 Oct 2018
 */
 
 $(document).ready( function() {
-	
+
 	$('.search-ico').ready( function() {
 		$('.search-ico').click( function() {
-			$('.searchbar-wrapper').css('display','block');
+			$('.searchbar-wrapper').slideDown();
+			$('.searchbar-wrapper').fadeIn(500);
 			$('.outer-search-ico').hide();
-			$('.search-txt').css('width','auto');
+
+			$('.outer-search-ico').replaceWith('<i class="fas search-ico fas-global-ico close-searchbar fa-2x fa-times" style="color:#939393;" onclick="closeSearch()"></i>');
+
 		});
 	});
-	
+
 });
 
+
+function closeSearch()
+{
+	console.log("IN");
+	$('.searchbar-wrapper').slideUp();
+	$(".searchbar-wrapper").fadeOut();
+	$('.close-searchbar').replaceWith('<i class="fas fa-search fa-2x search-ico fas-global-ico outer-search-ico" style="color:#939393;"></i>');
+}
