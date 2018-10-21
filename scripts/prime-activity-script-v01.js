f0f5c9fc9653f936fae513c639d577efbce35eef
itassistors <itassistors2013@gmail.com>
2018-10-21 20:21:46 +0530

    Script Change
    Consolidated all ng-clicks under one function with parameter

--- scripts/prime-activity-script-v01.js
+++ scripts/prime-activity-script-v01.js
@@ -8,48 +8,57 @@ Date : 19 Oct 2018
 
 var bingeBufferApp = angular.module('BingeBuffer',[]);
 
-bingeBufferApp.controller('menuController',['$scope', function($scope){
+bingeBufferApp.controller('globalController', ['$scope', function($scope){
 	
-	$scope.catListToggle = function() {
-		$('.category-list-wrapper').toggle("slide", {direction: 'up'}, 300);
+	$scope.clickActivator = function(bundle){
+		
+		if( bundle == 'searchSwitch' )
+		{
+			$('.searchbar-wrapper').slideToggle(500);
+		
+			if( $scope.switchSearch == undefined )
+			{
+				$('.outer-search-ico').attr('style','color: #FF2F52');
+				$scope.switchSearch = 0;
+			}
+			else if( $scope.switchSearch == 0 )
+			{
+				$('.outer-search-ico').attr('style','color: #939393');
+				$scope.switchSearch = undefined;
+			}
+		}
+		else if( bundle == 'authSwitch' )
+		{
+			$('.auth-bubble').slideToggle(500);
+		
+			if( $scope.authSwitch == undefined )
+			{
+				$('.user-ico').attr('style','color: #2873FF');
+				$scope.authSwitch = 0;
+			}
+			else if( $scope.authSwitch == 0 )
+			{
+				$('.user-ico').attr('style','color: #939393');
+				$scope.authSwitch = undefined;
+			}
+		}
+		else if( bundle == 'categorySwitch' )
+		{
+			$('.category-list-wrapper').toggle("slide", {direction: 'up'}, 300);
+			
+			if( $scope.catSwitch == undefined )
+			{
+				$('.category-menu').attr('style','color: #000');
+				$scope.catSwitch = 0;
+			}
+			else if( $scope.catSwitch == 0 )
+			{
+				$('.category-menu').removeAttr('style');
+				$scope.catSwitch = undefined;
+			}
+		}
+		
 	};
 	
-}]);
-
-bingeBufferApp.controller('globalIcoController',['$scope', function($scope){
-	$scope.toggleSearchbar = function(){
-		
-		
-		$('.searchbar-wrapper').slideToggle(500);
-		
-		if( $scope.switchSearch == undefined )
-		{
-			$('.outer-search-ico').attr('style','color: #FF2F52');
-			$scope.switchSearch = 0;
-		}
-		else if( $scope.switchSearch == 0 )
-		{
-			$('.outer-search-ico').attr('style','color: #939393');
-			$scope.switchSearch = undefined;
-		}
-		
-	};
-	
-	$scope.toggleAuthBubble = function() {
-	
-		$('.auth-bubble').slideToggle(500);
-		
-		if( $scope.authSwitch == undefined )
-		{
-			$('.user-ico').attr('style','color: #2873FF');
-			$scope.authSwitch = 0;
-		}
-		else if( $scope.authSwitch == 0 )
-		{
-			$('.user-ico').attr('style','color: #939393');
-			$scope.authSwitch = undefined;
-		}
-	
-	};
 }]);
 
